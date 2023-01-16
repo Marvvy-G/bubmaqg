8754205c297f7da7b4d6481cc16d187dc9662cef
Etukudoh Marvellous <Marvellous Etukudoh>
2023-01-16 21:12:20 +0100

    first commit

---
+++ src/components/Pages/Home.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import Header from '../Common/Header';
+import img from '../assets/img/header-bgbg.jpg';
+
+//Re-usable components
+import Services from '../Common/Services';
+import Portfolio from '../Common/Portfolio';
+import About from '../Common/About';
+import Footer from '../Common/Footer';
+
+class Home extends Component {
+    render(){
+        return(
+            <div>
+                <Header 
+                    title="Welcome To Bubmaq-G!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="Tell me more "
+                    link="/services"
+                    showButton={true}
+                    img= {img}
+                /> 
+                <Services />
+                <Portfolio />
+                <About />
+                <Footer /> 
+                
+            </div>
+        )
+    }
+}
+
+export default Home;
