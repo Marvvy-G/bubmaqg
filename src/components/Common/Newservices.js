f8cca2eba40137489edb8bfe8a6d45c5e11ef61b
Marvvy-G <57817706+Marvvy-G@users.noreply.github.com>
2023-07-30 00:45:41 +0100

    Update Newservices.js

--- src/components/Common/Newservices.js
+++ src/components/Common/Newservices.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import NewservicesItem from './NewservicesItem';
+
+import imgX from '../assets/img/portfolio/consultation.jpeg';
 import img1 from '../assets/img/portfolio/cake.jpg';
 import img2 from '../assets/img/portfolio/water.jpg';
 import img3 from '../assets/img/portfolio/general-contractor.jpg';
@@ -8,12 +10,13 @@ import img5 from '../assets/img/portfolio/livestock.jpg';
 import img6 from '../assets/img/portfolio/events-mmgt.jpg';
 
 const newservices = [
+    {title:'Consultation', subtitle:'Foreign Admission Processing, Visas Processing, Travel, and Tours processing', img:imgX},
     {title:'Catering', subtitle:'Pasteries, healthy Food, beverages', img:img1},
-    {title:'Water Production', subtitle:'Clean and healhty water in our community', img:img2},
+    {title:'Water Production', subtitle:'Clean and healthy water in our community, img:img2},
     {title:'General Contractor', subtitle:'Trading, supply of goods and services, real-estate rental services', img:img3},
-    {title:'Farming', subtitle:'Use of new farming techniques to produce healty fruits and vegetable', img:img4},
+    {title:'Farming', subtitle:'Use of new farming techniques to produce healthy fruits and vegetables', img:img4},
     {title:'Livestock', subtitle:'Our primary focus in the livestock industry is chicken (all breeds)', img:img5},
-    {title:'Events Management', subtitle:'We handle planning, decor and management for your events', img:img6}
+    {title:'Events Management', subtitle:'We handle planning, decor, and management for your events', img:img6}
 ];
 class Newservices extends Component {
     render(){
